2df54638db9c25aea718c1e0c10becd3ef172f35
singarilalitha <singarilalitha79@gmail.com>
2021-01-29 14:32:22 +0530

    new

---
+++ ammu.py
@@ -0,0 +1,5 @@
+x=input("enter the command")
+if x=="yes":
+	print("hi..,iam ammu")
+else:
+	print("byee....")
---
+++ workshop/lalli.py
@@ -0,0 +1,6 @@
+x=input(" enter the command")
+if x=="yes":
+	print("hi.. iam ammu")
+else:
+	print("byee...")
+
